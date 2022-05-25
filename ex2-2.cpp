95844af68ec020ddbd7cda55997400e7cbb0dd24
Kuroyei <81549868+kuroyei@users.noreply.github.com>
2022-05-25 16:25:03 +0900

    Add files via upload

--- ex2-2.cpp
+++ ex2-2.cpp
@@ -10,31 +10,6 @@ using namespace std;
 char* strncopy(char *dst, const char *src, unsigned count){
   // copy src[<=count-1] to dst
 
-  /* 解説
-
-    i は 何番目であるかを示す
-    入出力を見てみよう
-    in : ABCDEFGHIJKLMNOPQURSTVUWXYZ 5
-    out: ABCDE
-
-    最初の5文字が表示されている
-    ここで，Aは0番目，...，Eは4番目
-    すなわち，5番目まで表示すればよい
-    よって，i < count という条件がある
-
-    また，問題には「元の文字列srcの文字数がcountより少ない場合には、文字列全体をコピーするとする」とある
-    i < count という条件だけでは駄目なのである
-    もし，i が srcの文字列 を上回ると，*(str+i) == '\0' を満たすことになる．
-    よって，while() の条件に *(str+i) == '\0' がある
-
-    && としないと，*(str+i) == '\0' であっても while() が実行されるため注意である
-
-    while()の後，配列の最後に '\0' を代入している．
-    i の値が増えてから while() は閉じるから，i は dst 配列の最後を指していると思ってよい
-    
-
-  */
-
   int i = 0;
   while(i < count && *(src+i) != '\0'){
     dst[i] = src[i];
